17110edd2979be3872c1ba36b6ed1d3d8799a5d3
Kévin Walcarius <kevin.walcarius@gmail.com>
2025-07-21 17:35:32 +0200

    Base enums for input data.

---
+++ src/cams_iop/structs.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Specie(str, Enum):
+    ZERODEUX = "zerodeux"
+    SULFATE_CAMS = "sulphate"
+    SEA_SALT_CAMS = "sea_salt"
+    DUST_CAMS = "dust"
+    BLACK_CARBON_CAMS = "black_carbon"
+    NITRATE_CAMS = "nitrate"
+    AMMONIUM_CAMS = "ammonium"
+    ORGANIC_MATTER_CAMS = "organic_matter"
+
+class GranuMode(Enum):
+    MONO_MODAL = 1
+    BI_MODAL = 2
+
+class CamsVersion(str, Enum):
+    pass
